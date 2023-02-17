cabb6254da4a4680d3b52ae5277c1cd838133f0e
wit4401 <wtownsend4401@gmail.com>
2023-02-17 11:08:14 -0500

    added lab1 assignment

---
+++ Labs/Lab1/lab1.py
@@ -0,0 +1,34 @@
+"""
+COSC 311 Lab 1
+
+The attached is a Science paper (.txt file) published in Feb. 9, 2023. Write a Python program 
+to analyze this paper and answer the following questions.
+"""
+
+"""
+Task 1: How many different words appeared in this paper?
+"""
+
+"""
+Task 2: What are the 10 words that appear most frequently (from high to low)? 
+"""
+
+
+"""
+Task 3: What are the appearance frequencies for the following words?
+    - Summerfelt 
+    - wastewater
+    - greenhouse 
+    - salmon
+"""
+
+
+"""
+Task 4: What are the words appear exactly 1 time, 2 times, 5 times, and 10 times, respectively?
+"""
+
+
+
+"""
+Task 5: Draw a bar figure to show the average length of words for each appearance frequency.
+"""
