d646163f307358a08974dce1e5cc021753f41598
wit4401 <wtownsend4401@gmail.com>
2023-03-06 16:07:09 -0500

    updated hw 2

--- Homework/HW2/hw02.py
+++ Homework/HW2/hw02.py
@@ -1,9 +1,9 @@
 """
 Homework 2: Tic-Tac-Toe
-
-Create a two player text version of Tic-Tac-Toe.
-
+A two player text version of Tic-Tac-Toe.
 """
+
+# display tic tac toe board
 def display_grid(board):
     row = 0
     for i in range(9):
@@ -16,6 +16,7 @@ def display_grid(board):
                 print('-'*5)
             row+=1
 
+# check if there is a winner
 def check_winner(board):
     win=False
     if board[0][0]==board[0][1] and board[0][1]==board[0][2]:
@@ -35,6 +36,7 @@ def check_winner(board):
             win=True
     return win
 
+# checks for a tied game 'i.e. the board is full'
 def check_tie(board):
     tie=True
     for row in board:
@@ -42,7 +44,8 @@ def check_tie(board):
             if val==' ':
                 tie=False
     return tie
-              
+
+# main of the program         
 if __name__=="__main__":
     game_grid = [[' ',' ',' '],
                  [' ',' ',' '],
@@ -63,9 +66,10 @@ if __name__=="__main__":
             game_grid[row][col]=player
             win=check_winner(game_grid)
             tie=check_tie(game_grid)
-
+            
             print()
             display_grid(game_grid)
+
             if win:
                 print("\nWinner is " + player + "!")
                 flag=False
