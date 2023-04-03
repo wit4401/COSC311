fa64554f84cc61f1645ddd828ca5fe154ac0aa42
wit4401 <wtownsend4401@gmail.com>
2023-04-03 09:38:33 -0400

    added project files

---
+++ Project/project1.py
@@ -0,0 +1,15 @@
+# K Nearest Neighbor Libraries 
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
+# Decision Tree libraries
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
+
+# More Necessary Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+sns.set()
+
+
