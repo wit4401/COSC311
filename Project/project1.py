5f2d5748fd3bf75d6e12ffef44c87edbbbc6d078
wit4401 <wtownsend4401@gmail.com>
2023-04-05 11:05:52 -0400

    set up for project tasks

--- Project/project1.py
+++ Project/project1.py
@@ -13,4 +13,22 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 sns.set()
 
+# set up data for Classification of wifi localization
+wifi_data=pd.read_csv('wifi_localization.txt',sep="\t",names=['S1','S2','S3','S4','S5','S6','S7','Room#'])
+print(wifi_data.info())
 
+"""
+Part I: Self-test for KNN and DT algorithms
+"""
+room1=wifi_data[wifi_data['Room#']==1]
+room2=wifi_data[wifi_data['Room#']==2]
+room3=wifi_data[wifi_data['Room#']==3]
+room4=wifi_data[wifi_data['Room#']==4]
+
+"""
+Part II: Independent-test for KNN and DT algorithms
+"""
+
+"""
+Part III: Classification Model Finalization
+"""
