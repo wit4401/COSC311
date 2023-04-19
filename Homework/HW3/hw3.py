4b68dd7bf4126a93d36aacbc418a64de3c6adb7f
Will Townsend <wtownsend4401@gmail.com>
2023-04-19 10:50:03 -0400

    updated hw 3 to display average accuracies in plot

--- Homework/HW3/hw3.py
+++ Homework/HW3/hw3.py
@@ -10,19 +10,34 @@ Y=food_data['Class'].values
 x_train,x_test,y_train,y_test=train_test_split(X,Y,test_size=0.2,random_state=0)
 
 from sklearn.neighbors import KNeighborsClassifier
-
-model_1=KNeighborsClassifier(n_neighbors=2)
-model_1.fit(x_train,y_train)
-print(classification_report(y_test, model_1.predict(x_test)))
+accurs = []
+for i in range(1,101):
+    model_1=KNeighborsClassifier(n_neighbors=i)
+    model_1.fit(x_train,y_train)
+    accurs.append(model_1.score(x_test,y_test))
+#print(classification_report(y_test, model_1.predict(x_test)))
+plt.figure()
+plt.plot(list(range(1,101)),accurs)
+plt.show()
 
 from sklearn.tree import DecisionTreeClassifier
-
-model_2=DecisionTreeClassifier(random_state=0)
-model_2.fit(x_train,y_train)
-print(classification_report(y_test, model_2.predict(x_test)))
+accurs=[]
+for i in range(1,21):
+    model_2=DecisionTreeClassifier(max_depth=i,random_state=0)
+    model_2.fit(x_train,y_train)
+    accurs.append(model_2.score(x_test,y_test))
+#print(classification_report(y_test, model_2.predict(x_test)))
+plt.figure()
+plt.plot(list(range(1,21)),accurs)
+plt.show()
 
 from sklearn.ensemble import RandomForestClassifier
-
-model_3= RandomForestClassifier(n_estimators=750,max_depth=25,random_state=0)
-model_3.fit(x_train,y_train)
-print(classification_report(y_test, model_3.predict(x_test)))
+accurs=[]
+for i in range(1,1000):
+    model_3= RandomForestClassifier(n_estimators=i,max_depth=25,random_state=0)
+    model_3.fit(x_train,y_train)
+    accurs.append(model_3.score(x_test,y_test))
+#print(classification_report(y_test, model_3.predict(x_test)))
+plt.figure()
+plt.plot(list(range(1,1000)),accurs)
+plt.show()
